7bcac9ca7e5947aa9fbc53758780129dc53f6ed0
Ali <aliammarali125@gmail.com>
2021-10-06 21:19:20 +0300

    fix db on phone

--- lib/db/database.dart
+++ lib/db/database.dart
@@ -9,6 +9,7 @@ import 'package:moor/ffi.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
 import 'package:riverpod/riverpod.dart';
+import 'package:moor_flutter/moor_flutter.dart';
 part 'database.g.dart';
 
 final dbProvider = Provider((_) => AppDb());
@@ -78,7 +79,13 @@ class AppDb extends _$AppDb {
 LazyDatabase _openConnection() {
   return LazyDatabase(() async {
     final dbFolder = await getApplicationDocumentsDirectory();
-    final file = File(p.join(dbFolder.path, 'db.sqlite'));
-    return VmDatabase(file);
+    var join = p.join(dbFolder.path, 'db.sqlite');
+
+    if (Platform.isLinux || Platform.isWindows) {
+      final file = File(join);
+      return VmDatabase(file);
+    } else {
+      return FlutterQueryExecutor(path: join);
+    }
   });
 }
