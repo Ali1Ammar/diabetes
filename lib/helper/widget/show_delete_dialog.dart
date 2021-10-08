aae74672f30516e3666e30b7960f93248ff2c90a
Ali <aliammarali125@gmail.com>
2021-10-09 00:11:12 +0300

    fix text of dialog button

--- lib/helper/widget/show_delete_dialog.dart
+++ lib/helper/widget/show_delete_dialog.dart
@@ -5,13 +5,13 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 showDeleteDialog(BuildContext context, VoidCallback onYes) => showDialog(
     context: context,
     builder: (context) => AlertDialog(
-          content:  Text(AppLocalizations.of(context)!.areyousuredelete  ),
+          content: Text(AppLocalizations.of(context)!.areyousuredelete),
           actions: [
             TextButton(
                 onPressed: () {
                   Navigator.pop(context);
                 },
-                child:  Text(AppLocalizations.of(context)!.yesdelete )),
+                child: Text(AppLocalizations.of(context)!.cancel)),
             ElevatedButton(
                 style: ElevatedButton.styleFrom(primary: Colors.red),
                 onPressed: () {
@@ -19,6 +19,6 @@ showDeleteDialog(BuildContext context, VoidCallback onYes) => showDialog(
 
                   onYes();
                 },
-                child:  Text(AppLocalizations.of(context)!.cancel ))
+                child: Text(AppLocalizations.of(context)!.yesdelete))
           ],
         ));
